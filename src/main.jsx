33084dda674a622717bd8267565dd7851b067956
dkaliazin <dkaliazin@gmail.com>
2024-08-18 20:55:12 +0300

    without persistgate

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App'
-import './index.css'
-import { PersistGate } from 'redux-persist/integration/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
+import './index.css';
 import { Provider } from 'react-redux';
-import { store} from './redux/store';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
